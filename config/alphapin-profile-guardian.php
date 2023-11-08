908bbc2a2de9e6d8320fdd475478f923526272f6
Elminson De Oleo Baez <edeoleo@gmail.com>
2023-11-07 19:43:31 -0500

    Update pin_type option to include alpha_numeric option and remove unnecessary pin_special_chars options

--- config/alphapin-profile-guardian.php
+++ config/alphapin-profile-guardian.php
@@ -3,15 +3,11 @@
 // config for PHPDominicana/AlphapinProfileGuardian
 return [
 
-	'pin_type' => 'numeric', // numeric, alpha, alphanumeric
+	'pin_type' => 'numeric', // numeric, alpha, alpha_numeric
 	'pin_length' => 6,
 	'pin_case' => 'lower', // lower, upper, mixed
-	'pin_special_chars' => false,
-	'pin_special_chars_list' => '!@#$%^&*()_-=+{}[]',
-	'pin_special_chars_list_length' => 10,
-	'pin_special_chars_list_case' => 'lower', // lower, upper, mixed
-	'pin_special_chars_list_repeat' => false,
-	'pin_special_chars_list_repeat_min' => 1,
-	'pin_special_chars_list_repeat_max' => 2,
+	'use_special_chars' => false,
+	'pin_additional_special_chars_list' => '.,;:!?', // you can add more special chars here
+	'enable_special_chars_repeat' => false,
 
 ];
--- src/AlphapinProfileGuardian.php
+++ src/AlphapinProfileGuardian.php
@@ -5,4 +5,47 @@ namespace PHPDominicana\AlphapinProfileGuardian;
 class AlphapinProfileGuardian
 {
 
+	/**
+	 * @var \Illuminate\Config\Repository|\Illuminate\Contracts\Foundation\Application|\Illuminate\Foundation\Application|mixed
+	 */
+	private $pinType;
+	private $pinLength;
+
+	private $pinChars = [
+		'numeric_chars' => '0123456789',
+		'alpha_chars' => 'abcdefghijklmnopqrstuvwxyz',
+		'alpha_numeric_chars' => 'abcdefghijklmnopqrstuvwxyz0123456789',
+		'pin_special_chars' => '!@#$%^&*()_-=+{}[]'
+	];
+	/**
+	 * @var \Illuminate\Config\Repository|\Illuminate\Contracts\Foundation\Application|\Illuminate\Foundation\Application|mixed
+	 */
+	protected $enableSpecialCharsRepeat;
+	/**
+	 * @var \Illuminate\Config\Repository|\Illuminate\Contracts\Foundation\Application|\Illuminate\Foundation\Application|mixed
+	 */
+	protected $useSpecialChars;
+	/**
+	 * @var \Illuminate\Config\Repository|\Illuminate\Contracts\Foundation\Application|\Illuminate\Foundation\Application|mixed
+	 */
+	protected $pinCase;
+
+	public function __construct()
+	{
+		$this->pinType = config('alphapin-profile-guardian.pin_type');
+		$this->pinLength = config('alphapin-profile-guardian.pin_length');
+		$this->pinCase = config('alphapin-profile-guardian.pinLength');
+		$this->useSpecialChars = config('alphapin-profile-guardian.use_special_chars');
+		$this->enableSpecialCharsRepeat = config('alphapin-profile-guardian.enable_special_chars_repeat');
+
+	}
+
+	public function generatePIN()
+	{
+
+		// read from config and generate the PIN
+		// return the PIN
+
+	}
+
 }
