9a4fb4f1d176177820913f7257c6e4619c1b90a8
Elminson De Oleo Baez <edeoleo@gmail.com>
2023-11-07 19:25:18 -0500

    Add initial configuration options for pin generation in AlphapinProfileGuardian

--- config/alphapin-profile-guardian.php
+++ config/alphapin-profile-guardian.php
@@ -3,4 +3,15 @@
 // config for PHPDominicana/AlphapinProfileGuardian
 return [
 
+	'pin_type' => 'numeric', // numeric, alpha, alphanumeric
+	'pin_length' => 6,
+	'pin_case' => 'lower', // lower, upper, mixed
+	'pin_special_chars' => false,
+	'pin_special_chars_list' => '!@#$%^&*()_-=+{}[]',
+	'pin_special_chars_list_length' => 10,
+	'pin_special_chars_list_case' => 'lower', // lower, upper, mixed
+	'pin_special_chars_list_repeat' => false,
+	'pin_special_chars_list_repeat_min' => 1,
+	'pin_special_chars_list_repeat_max' => 2,
+
 ];
--- src/AlphapinProfileGuardian.php
+++ src/AlphapinProfileGuardian.php
@@ -4,4 +4,5 @@ namespace PHPDominicana\AlphapinProfileGuardian;
 
 class AlphapinProfileGuardian
 {
+
 }
