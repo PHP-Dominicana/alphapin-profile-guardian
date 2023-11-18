3081434e6c371328d7afb94d85b5f16eec3f883b
Elminson De Oleo Baez <edeoleo@gmail.com>
2023-11-18 12:54:38 -0500

    Add User model and tests for generating pin and sending email

--- tests/Feature/AlphapinProfileGuardianTest.php
+++ tests/Feature/AlphapinProfileGuardianTest.php
@@ -1,20 +1,39 @@
 <?php
 
+use \App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPDominicana\AlphapinProfileGuardian\AlphapinProfileGuardian;
 use PHPDominicana\AlphapinProfileGuardian\Database\Factories\UserFactory;
 use PHPDominicana\AlphapinProfileGuardian\Tests\TestCase;
-class User {
-	public int $id;
-	public string $name;
-	public string $email;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
-	public function __construct(int $id, string $name, string $email) {
-		$this->id = $id;
-		$this->name = $name;
-		$this->email = $email;
-	}
-}
+beforeEach(function () {
+	// Set up your SQLite in-memory database
+	$capsule = new Capsule;
+	$capsule->addConnection([
+								'driver'   => 'sqlite',
+								'database' => ':memory:',
+								'prefix'   => '',
+							]);
+	$capsule->setAsGlobal();
+	$capsule->bootEloquent();
+
+	// Run your migrations
+	include_once __DIR__.'/../../database/migrations/create_alphapin_profile_guardian_table.php.stub';
+
+});
+
+it('can create a new post', function () {
+	// Your test logic goes here
+	$user = User::create([
+					 'name' => 'John Doe',
+					 'email' => 'johndoe@emai.com',
+				 ]);
+
+	// Assertions go here
+	$this->assertDatabaseCount('user', 1);
+	$this->assertDatabaseHas('user', ['name' => 'John Doe']);
+});
 
 test('testing generate pin (feature)', function () {
 
@@ -27,12 +46,16 @@ test('testing generate pin (feature)', function () {
 it('can send email', function () {
 	uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
-	\Pest\Laravel\artisan('migrate:fresh');
+	// \Pest\Laravel\artisan('migrate:fresh');
 
-	// $stub = $this->createMock(User::class);
-	// $user = UserFactory::new()->create();
+	$stub = $this->createMock(User::class);
+	$user = UserFactory::new()->create();
 	// print_r($user);
-	$user = new \User(1, 'John Doe', 'john@doe.com');
+	// $user = new User(1, 'John Doe', 'john@doe.com');
+	// $user = User::create([
+	// 						 'name' => 'John Doe',
+	// 						 'email' => 'john@doe.com',
+	// 					 ]);
 
 	$alphapinProfileGuardian = new AlphapinProfileGuardian();
 	return $alphapinProfileGuardian->sendPin(1, $user);
